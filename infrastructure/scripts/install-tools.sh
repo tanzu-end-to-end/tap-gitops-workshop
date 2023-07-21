e5fb465b467d73676b306ec7c957c6089a31b57e
pvtl-pre <>
2023-07-21 13:45:36 -0500

    Removed docker as it's not needed

--- infrastructure/scripts/install-tools.sh
+++ infrastructure/scripts/install-tools.sh
@@ -19,24 +19,6 @@ wget -O- https://carvel.dev/install.sh | sudo bash
 sudo apt-get install direnv
 eval "$(direnv hook bash)"
 
-# docker
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
-
 # git
 sudo add-apt-repository ppa:git-core/ppa -y
 sudo apt-get update
