0bd318b8a73e09a820513cf04c2bae220c53c37d
leigh capili <leigh@null.net>
2023-07-25 19:41:24 +0000

    Update apt for JDK install

--- infrastructure/scripts/install-tools.sh
+++ infrastructure/scripts/install-tools.sh
@@ -37,7 +37,8 @@ sudo apt install -y wget apt-transport-https
 sudo mkdir -p /etc/apt/keyrings
 wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /etc/apt/keyrings/adoptium.asc
 echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
-sudo apt install temurin-17-jdk
+sudo apt update
+sudo apt install temurin-17-jdk -y
 
 # jq
 sudo apt-get --assume-yes install jq
