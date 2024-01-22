426b32994de119b815f58b892df9f106d2879ccd
Oded Shopen <odedia@gmail.com>
2024-01-22 21:52:32 +0200

    temurin install seems to be returning 403 for a few weeks now. Switched to openjdk

--- infrastructure/scripts/install-tools.sh
+++ infrastructure/scripts/install-tools.sh
@@ -41,7 +41,7 @@ sudo mkdir -p /etc/apt/keyrings
 wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /etc/apt/keyrings/adoptium.asc
 echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
 sudo apt update
-sudo apt install temurin-17-jdk -y
+sudo apt install openjdk-17-jdk -y
 
 # jq
 sudo apt-get --assume-yes install jq
